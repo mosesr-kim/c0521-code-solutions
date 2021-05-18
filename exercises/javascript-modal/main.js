0a649c314fab0c0f35897ca00e247dbeff5b120a
Moses Kim <mosesrandykim@gmail.com>
2021-05-18 16:51:20 -0700

    made changes

--- exercises/javascript-modal/main.js
+++ exercises/javascript-modal/main.js
@@ -2,20 +2,13 @@ var $openModal = document.querySelector('.openModal');
 var $modal = document.querySelector('.modalHidden');
 var $noButton = document.querySelector('.noButton');
 var $modalContainer = document.querySelector('.modalContainerHidden');
-var isOpen = false;
 function open(event) {
-  if (isOpen === false) {
-    $modal.className = 'modalOpen';
-    $modalContainer.className = 'modalContainerOpen';
-    isOpen = true;
-  }
+  $modal.className = 'modalOpen';
+  $modalContainer.className = 'modalContainerOpen';
 }
 $openModal.addEventListener('click', open);
 function close(event) {
-  if (isOpen === true) {
-    $modal.className = 'modalHidden';
-    $modalContainer.className = 'modalContainerHidden';
-    isOpen = false;
-  }
+  $modal.className = 'modalHidden';
+  $modalContainer.className = 'modalContainerHidden';
 }
 $noButton.addEventListener('click', close);
