7d74a3c2ca72678c049e9d8e793fc2e4a460d0f5
Moses Kim <mosesrandykim@gmail.com>
2021-05-18 21:28:40 -0700

    completed object-manipulation-2

--- exercises/object-manipulation-2/src/defaults.js
+++ exercises/object-manipulation-2/src/defaults.js
@@ -1,12 +1,9 @@
 /* exported defaults */
 function defaults(target, source) {
-  debugger
   for (var keySource in source) {
-    truth = false;
-    for (var keyTarget in target) {
-      if (keySource in target) {
-        truth = true;
-      }
+    var truth = false;
+    if (keySource in target) {
+      truth = true;
     }
     if (truth === false) {
       target[keySource] = source[keySource];
--- exercises/object-manipulation-2/src/pick.js
+++ exercises/object-manipulation-2/src/pick.js
@@ -2,12 +2,9 @@
 function pick(source, keys) {
   var newObject = {};
   for (var i = 0; i < keys.length; i++) {
-    if (keys[i] in source) {
+    if ((keys[i] in source) && (source[keys[i]] !== undefined)){
       var newValue = source[keys[i]];
       newObject[keys[i]] = newValue;
-    }
-    if (newValue === undefined) {
-      delete newObject[keys[i]];
     }
   }
   return newObject;
