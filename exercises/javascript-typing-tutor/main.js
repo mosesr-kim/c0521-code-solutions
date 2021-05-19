5d688019b147d6ce43254891f81b280205770569
Moses Kim <mosesrandykim@gmail.com>
2021-05-18 17:58:37 -0700

    end of class tuesday

---
+++ exercises/javascript-typing-tutor/main.js
@@ -0,0 +1,16 @@
+// var $span = document.querySelectorAll('span');
+// var spanArray = Array.from($span);
+
+// console.log("spanArray:", spanArray);
+// console.log("$span:", $span);
+
+// document.addEventListener('keydown', logKey);
+// function logKey(event) {
+// console.log(event.key);
+// }
+
+// console.log("spanArray[0].textContent:", spanArray[0].textContent);
+// console.log("spanArray[0].textContent.toUpperCase():", spanArray[0].textContent.toUpperCase());
+// console.log("'Key' + spanArray[0].textContent.toUpperCase()):", "Key" + spanArray[0].textContent.toUpperCase());
+// console.log("spanArray[0] === 'g':", spanArray[0] === 'g');
+// console.log("spanArray[0]:", spanArray[0]);
