15e8675336f3e91951e0bbbb189c696751cc4e1d
Moses Kim <mosesrandykim@gmail.com>
2021-06-02 17:24:28 -0700

    in progress

---
+++ exercises/object-manipulation-3/main.js
@@ -0,0 +1,16 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+var players = [
+  {
+    name: 'player 1',
+    hand: ''
+  }, {
+    name: 'player 2',
+    hand: ''
+  }, {
+    name: 'player 3',
+    hand: ''
+  }, {
+    name: 'player 4',
+    hand: ''
+  }
+];
