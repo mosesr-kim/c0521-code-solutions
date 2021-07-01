df0710eb2f644d1840aaec97d50c8ec44800cb48
mosesr-kim <mosesrandykim@gmail.com>
2021-07-01 00:03:44 +0000

    completed sql-join

---
+++ exercises/sql-join/magic-mallrats.sql
@@ -0,0 +1,6 @@
+select "firstName", "lastName"
+  from "films"
+  join "inventory" using ("filmId")
+  join "rentals" using ("inventoryId")
+  join "customers" using ("customerId")
+ where "title" = 'Magic Mallrats';
