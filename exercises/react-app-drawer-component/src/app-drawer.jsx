4e6f4598eb832960cc172de64209006e8ea1ea5d
Moses Kim <mosesrandykim@gmail.com>
2021-07-12 19:52:34 +0000

    in progress

---
+++ exercises/react-app-drawer-component/src/app-drawer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+export default class AppDrawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
