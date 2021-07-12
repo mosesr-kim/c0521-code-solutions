1645cc34adcaf20d6f4aa3b960867b90f4603b52
Moses Kim <mosesrandykim@gmail.com>
2021-07-12 23:22:33 +0000

    in progress

--- exercises/react-app-drawer-component/src/app-drawer.jsx
+++ exercises/react-app-drawer-component/src/app-drawer.jsx
@@ -2,21 +2,36 @@ import React from 'react';
 export default class AppDrawer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { isOpen: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    if (this.state.isOpen === false) {
+      this.setState({ isOpen: true });
+    } else {
+      this.setState({ isOpen: false });
+    }
   }
 
   render() {
+    let isOpen = null;
+    if (this.state.isOpen === true) {
+      isOpen = 'drawer';
+    } else {
+      isOpen = 'drawer hidden';
+    }
     return (
       <>
       <div>
-        <i className="fas fa-bars navBar"></i>
+        <i className="fas fa-bars navBar" onClick={ this.handleClick }></i>
       </div>
       <div>
-        <div className="drawer">
-          <h2>Menu</h2>
-          <h3>About</h3>
-          <h3>Get Started</h3>
-          <h3>Sign In</h3>
+        <div className={ isOpen }>
+          <h2 className="menu" onClick={this.handleClick}>Menu</h2>
+          <h3 className="about" onClick={this.handleClick}>About</h3>
+          <h3 className="getStarted" onClick={this.handleClick}>Get Started</h3>
+          <h3 className="signIn" onClick={this.handleClick}>Sign In</h3>
         </div>
       </div>
       </>
