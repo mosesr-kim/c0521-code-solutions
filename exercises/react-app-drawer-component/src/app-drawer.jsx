119011df44c2d6faf942e09de97df4f96d67795b
Moses Kim <mosesrandykim@gmail.com>
2021-07-12 22:51:19 -0700

    completed react-app-drawer-component

--- exercises/react-app-drawer-component/src/app-drawer.jsx
+++ exercises/react-app-drawer-component/src/app-drawer.jsx
@@ -7,33 +7,21 @@ export default class AppDrawer extends React.Component {
   }
 
   handleClick() {
-    if (this.state.isOpen === false) {
-      this.setState({ isOpen: true });
-    } else {
-      this.setState({ isOpen: false });
-    }
+    this.state.isOpen ? this.setState({ isOpen: false }) : this.setState({ isOpen: true });
   }
 
   render() {
-    let isOpen = null;
-    if (this.state.isOpen === true) {
-      isOpen = 'drawer';
-    } else {
-      isOpen = 'drawer hidden';
-    }
+    const isOpen = this.state.isOpen ? '' : 'hidden';
     return (
       <>
-      <div>
-        <i className="fas fa-bars navBar" onClick={ this.handleClick }></i>
+      <i className="fas fa-bars navBar" onClick={ this.handleClick }></i>
+      <div className={`drawer ${isOpen}`}>
+        <h2 className="menu" onClick={this.handleClick}>Menu</h2>
+        <h3 className="about" onClick={this.handleClick}>About</h3>
+        <h3 className="getStarted" onClick={this.handleClick}>Get Started</h3>
+        <h3 className="signIn" onClick={this.handleClick}>Sign In</h3>
       </div>
-      <div>
-        <div className={ isOpen }>
-          <h2 className="menu" onClick={this.handleClick}>Menu</h2>
-          <h3 className="about" onClick={this.handleClick}>About</h3>
-          <h3 className="getStarted" onClick={this.handleClick}>Get Started</h3>
-          <h3 className="signIn" onClick={this.handleClick}>Sign In</h3>
-        </div>
-      </div>
+      <div className={`dimBackground ${isOpen}`} onClick={this.handleClick}></div>
       </>
     );
   }
