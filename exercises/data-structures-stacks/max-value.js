232683f90bba98d39e703338439dceff5bf800e0
Moses Kim <mosesrandykim@gmail.com>
2021-08-02 20:23:41 -0700

    completed data-structures-stacks

--- exercises/data-structures-stacks/max-value.js
+++ exercises/data-structures-stacks/max-value.js
@@ -1,5 +1,19 @@
 /* exported maxValue */
 
 function maxValue(stack) {
-
+  if (stack.peek() === undefined) {
+    return -Infinity;
+  } else {
+    const array = [];
+    while (stack.peek() !== undefined) {
+      array.push(stack.pop());
+    }
+    let highest = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] > highest) {
+        highest = array[i];
+      }
+    }
+    return highest;
+  }
 }
